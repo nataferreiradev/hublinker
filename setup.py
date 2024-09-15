fb2cd4c6bd923d3469390e8bee6ec09fe532723e
nataferreiradev <nataferreira929@hotmail.com>
2024-09-15 15:21:07 -0300

    add: __init__.py files fix: setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,7 @@ setup(
             'hublinker=hublinker.cli:main',  
         ],
     },
-    author='Seu Nome',
     description='hublinker: A CLI tool for interacting with the GitHub API directly from the terminal, making listings, cloning, and more easy',
-    url='https://github.com/seuusuario/hublinker',
     license='MIT',
     classifiers=[
         'Programming Language :: Python :: 3',
